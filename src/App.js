30b60406f2dee99d45ff1da872c162eb847a95c1
gundo5 <pakgundo5@gmail.com>
2021-07-15 10:38:07 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+
+const App = () => {
+  // useState 
+  // 배열 return 
+  // item-현재 상태, setItem-Setter함수
+  const [item, setItem] = useState(1); 
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{item}</h1>
+      <button onClick={incrementItem}>incr</button>
+      <button onClick={decrementItem}>decr</button>
     </div>
   );
-}
+};
 
 export default App;
